cbb5b97c22d3ab64dee11dafa5c3b86f1a34c2fc
Kyaw Han <kyawsoehan@tdmestech.app>
2022-03-29 02:34:54 -0400

    tests

--- sandbox/test.php
+++ sandbox/test.php
@@ -2,17 +2,17 @@
     include('../config/db_connection.php');
     include('../functions/functions.php');
     include('../variables/variables.php');
-    // $sql = "INSERT INTO tbl_user_order(user_id) VALUE(1);";
-    // if(mysqli_query($conn, $sql)){
-    //     $new_order_id = mysqli_insert_id($conn);
-    //     echo "Last inserted order id was " . $new_order_id;
-    //     $sql = "INSERT INTO tbl_order_history(order_id, book_id, amount, book_count) VALUES('$new_order_id', '1', '23.44', '5'), ('$new_order_id', '2', '23.44', '2'),('$new_order_id', '3', '12.39', '1'),('$new_order_id', '4', '12.48', '2'); ";
+    $sql = "INSERT INTO tbl_user_order(user_id) VALUE(1);";
+    if(mysqli_query($conn, $sql)){
+        $new_order_id = mysqli_insert_id($conn);
+        echo "Last inserted order id was " . $new_order_id;
+        $sql = "INSERT INTO tbl_order_history(order_id, book_id, amount, book_count) VALUES('$new_order_id', '1', '23.44', '5'), ('$new_order_id', '2', '23.44', '2'),('$new_order_id', '3', '12.39', '1'),('$new_order_id', '4', '12.48', '2'); ";
 
-    //     if(mysqli_query($conn, $sql)){
-    //         echo "Successfully inserted";
-    //     } else{
-    //         echo "insert to order history error";
-    //     }
+        if(mysqli_query($conn, $sql)){
+            echo "Successfully inserted";
+        } else{
+            echo "insert to order history error";
+        }
 
         
     // } else {
@@ -30,7 +30,7 @@
     //     return ($payment > 0) ? FALSE : TRUE;
     // }
 
-    check_if_address_exists($conn);
+    $has_address = check_if_address_exists($conn);
     
     
     function check_if_address_exists($conn){
@@ -45,7 +45,7 @@
         if($address == 0){
             return False;
         }else{
-            $has_address = True;
+            
             return True;
         }
     }
